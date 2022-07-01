98b1380ae9095750db99b4bc345a3ffb2b6d2150
Qing201 <qingch1@outlook.com>
2022-06-30 17:18:38 -0700

    leetcode

---
+++ Algorithms/Leetcode/77.combinations.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=77 lang=python3
+#
+# [77] Combinations
+#
+
+# @lc code=start
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+# @lc code=end
+
