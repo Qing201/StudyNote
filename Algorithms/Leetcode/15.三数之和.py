b016097a47e0136483db5797b3acf26b26c7e1f8
Qing201 <qingch1@outlook.com>
2023-08-27 19:14:36 +0800

    1

--- Algorithms/Leetcode/15.三数之和.py
+++ Algorithms/Leetcode/15.三数之和.py
@@ -8,15 +8,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        left, mid, right = 0, 1, len(nums)-1
         solution = []
+        
+        for i in range(0, len(nums) - 2): 
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left, right = i+1, len(nums)-1
 
-        while mid > left and mid < right:
-            sum = nums[left] + nums[mid] + nums[right]
-            if sum == 0:
-                solution += [[nums[left], nums[mid], nums[right]]]
-            elif sum > 0:
-                
+
+            while left < right:                
+                sum = nums[i] + nums[left] + nums[right]
+                if sum < 0:
+                    left += 1
+                if sum > 0:
+                    right -= 1    
+                elif sum == 0:
+                    solution.append([nums[i], nums[left], nums[right]])
+                    right -= 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
+                    left += 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+        
+        return solution
 
 # @lc code=end
 
